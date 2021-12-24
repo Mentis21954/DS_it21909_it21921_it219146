eebbe1fd39c4f3fb531d6d13ea47c86f1ca07a48
BillDouk <it21921@hua.gr>
2021-12-24 16:06:59 +0300

    Initial Commit Spring Boot of Spring Initializr Project

---
+++ DistributedSystemsAssignment/src/main/java/gr/hua/dit/DistributedSystemsAssignment/DistributedSystemsAssignmentApplication.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.DistributedSystemsAssignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedSystemsAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistributedSystemsAssignmentApplication.class, args);
+	}
+
+}
---
+++ DistributedSystemsAssignment/src/test/java/gr/hua/dit/DistributedSystemsAssignment/DistributedSystemsAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.hua.dit.DistributedSystemsAssignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedSystemsAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
