220e9d0bba3cb628a19b2e7b39220d0cd63244a7
it21909 <80172209+it21909@users.noreply.github.com>
2022-01-02 22:20:51 +0200

    Add files via upload

---
+++ DistributedSystemsAssignment/src/test/java/gr/hua/dit/DistributedSystemsAssignment/citizen.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class citizen {
+	//sign in
+	//login
+	//logout
+	//import documents
+	//new file
+	//edit file
+}
---
+++ DistributedSystemsAssignment/src/test/java/gr/hua/dit/DistributedSystemsAssignment/oasaEmployee.java
@@ -0,0 +1,7 @@
+package com.example.demo;
+
+public class oasaEmployee {
+	//login
+	//logout
+	//approval
+}
