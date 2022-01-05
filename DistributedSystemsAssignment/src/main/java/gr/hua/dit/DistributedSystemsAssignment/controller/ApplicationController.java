0c320c150bdc423d0acc52b04508f907a78a4abe
BillDouk <it21921@hua.gr>
2022-01-05 13:53:22 +0300

    added new entity and started on Controllers

---
+++ DistributedSystemsAssignment/src/main/java/gr/hua/dit/DistributedSystemsAssignment/controller/ApplicationController.java
@@ -0,0 +1,14 @@
+package gr.hua.dit.DistributedSystemsAssignment.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("Applications")
+public class ApplicationController {
+
+	
+	
+	
+	
+}
