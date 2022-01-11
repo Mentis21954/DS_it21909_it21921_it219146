d8f8406117280450e3471852711a89a7fc001b8d
it21909 <80172209+it21909@users.noreply.github.com>
2022-01-11 20:00:51 +0200

    Update UserServiceImpl.java

--- DistributedSystemsAssignment/src/main/java/gr/hua/dit/DistributedSystemsAssignment/service/UserServiceImpl.java
+++ DistributedSystemsAssignment/src/main/java/gr/hua/dit/DistributedSystemsAssignment/service/UserServiceImpl.java
@@ -1,6 +1,12 @@
 package gr.hua.dit.DistributedSystemsAssignment.service;
 
+import java.util.Collection;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.BeanDefinitionDsl.Role;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -17,6 +23,9 @@ public class UserServiceImpl implements UserService{
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired 
+	private BCryptPasswordEncoder passwordEncoder;
 
 	@Override
 	public User save(UserRegistrationDto registrationDto) {
@@ -32,5 +41,17 @@ public class UserServiceImpl implements UserService{
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+		User user=userRepository.findByUsername(username);
+		if(user==null) {
+			throw new UsernameNotFoundException("Invalid username or password!");
+		}
+		return new org.springframework.security.core.userdetails.User(user.getUsername(),user.getPassword(),mapRolesToAuthorities(user.getRoles()));
+		
+	}
+	
+	//method to map roles to authorities
+	private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles){
+		return roles.stream().map(role->new SimpleGrantedAuthority(role.getName())).collect(Collectors.toList());	
+	}
+	
+}
