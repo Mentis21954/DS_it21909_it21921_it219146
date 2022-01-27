86b4fbfcdb23895265cc3f5da9bc24c56a715f2f
it21909 <80172209+it21909@users.noreply.github.com>
2022-01-27 21:54:29 +0200

    Update main.js

--- DistributedSystemsAssignment/src/main/resources/static/js/main.js
+++ DistributedSystemsAssignment/src/main/resources/static/js/main.js
@@ -8,19 +8,10 @@ $(document).ready(function(){
 
         var href = $(this).attr('href');
         $.get(href, function(application,status){
-            $('#nameEdit').val(nationality.name);
-            $('#lnameEdit').val(nationality.lname);
-            $('#emailEdit').val(nationality.email);
-            $('#bitrhDateEdit').val(nationality.birtDate);
-            $('#idEdit').val(nationality.idNumber);
-            $('#afmEdit').val(nationality.AFM);
-            $('#amkaEdit').val(nationality.AMKA);
-            $('#maritalEdit').val(nationality.maritalStatus);
-            $('#nationalityEdit').val(nationality.primaryNationality);
-            $('#cityEdit').val(nationality.city);
-            $('#unemploymentEdit').val(nationality.dateOfUnemployment);
-            $('#photoEdit').val(nationality.photo);
-            $('#incomeEdit').val(nationality.income);
+			$('#idEdit').val(users.user_id);
+            $('#usernameEdit').val(users.username);
+            $('#authorityEdit').val(authorities.name);
+            
         });        
 
         $('#editModal').modal();
