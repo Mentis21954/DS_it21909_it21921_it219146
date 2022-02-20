db830b8ebc97eddb192ea5183c8c657eda5c6ffe
it21909 <80172209+it21909@users.noreply.github.com>
2022-02-21 00:40:40 +0200

    Update AdminController.java

--- DistributedSystemsAssignment/src/main/java/gr/hua/dit/DistributedSystemsAssignment/controller/AdminController.java
+++ DistributedSystemsAssignment/src/main/java/gr/hua/dit/DistributedSystemsAssignment/controller/AdminController.java
@@ -1,13 +1,19 @@
 package gr.hua.dit.DistributedSystemsAssignment.controller;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
+import gr.hua.dit.DistributedSystemsAssignment.entity.Application;
 import gr.hua.dit.DistributedSystemsAssignment.entity.Authority;
 import gr.hua.dit.DistributedSystemsAssignment.entity.User;
 import gr.hua.dit.DistributedSystemsAssignment.service.UserService;
@@ -31,5 +37,21 @@ public class AdminController {
 		model.addAttribute("users", users);
 		
 		return "adminPage";
+	}
+	
+	@GetMapping("/oneUser/{id}")
+	public ModelAndView oneUser(@PathVariable (name="id") Integer id) {
+		ModelAndView validation=new ModelAndView("oneUser");
+		Optional<User> user = userService.getUser(id);
+		validation.addObject("user",user);
+		return validation;
+	}
+	
+	@PostMapping("/saveUser")
+	public String saveUser(@ModelAttribute("user") User user) {
+		Optional<User> u = userService.getUser(user.getId());
+		
+		userService.save(u);
+		return "redirect:/OAEDPage";
 	}
 }
