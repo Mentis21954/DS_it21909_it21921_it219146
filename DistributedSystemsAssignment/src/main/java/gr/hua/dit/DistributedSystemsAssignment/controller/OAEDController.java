1fca074df979d22898228384998bd967d6147484
it21909 <80172209+it21909@users.noreply.github.com>
2022-02-20 20:41:31 +0200

    Update OAEDController.java

--- DistributedSystemsAssignment/src/main/java/gr/hua/dit/DistributedSystemsAssignment/controller/OAEDController.java
+++ DistributedSystemsAssignment/src/main/java/gr/hua/dit/DistributedSystemsAssignment/controller/OAEDController.java
@@ -1,14 +1,23 @@
 package gr.hua.dit.DistributedSystemsAssignment.controller;
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
 import gr.hua.dit.DistributedSystemsAssignment.entity.Application;
+import gr.hua.dit.DistributedSystemsAssignment.entity.Unemployed;
 import gr.hua.dit.DistributedSystemsAssignment.service.ApplicationService;
+import gr.hua.dit.DistributedSystemsAssignment.service.UnemployedsService;
 
 
 @Controller
@@ -28,7 +37,26 @@ public class OAEDController {
 		
 		List<Application>applications=applicationService.getApplications();
 		model.addAttribute("applications", applications);
+		
 		return "OAEDPage";
+	}
+	
+	
+	
+	
+	
+	@GetMapping("/validationForm/{id}")
+	public ModelAndView validationForm(@PathVariable (name="id") Integer id) {
+		ModelAndView validation=new ModelAndView("validationForm");
+		Application application = applicationService.getApplication(id);
+		validation.addObject("application",application);
+		return validation;
+	}
+	
+	@PostMapping("/saveValidation")
+	public String saveValidation(@ModelAttribute("application") Application application) {
+		applicationService.saveApplication(application);
+		return "redirect:/";
 	}
 	
 
