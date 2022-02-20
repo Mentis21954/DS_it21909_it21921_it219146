b2a8552e57c2b54f83036640b7fc8c56c0db5eb9
it21909 <80172209+it21909@users.noreply.github.com>
2022-02-21 00:42:55 +0200

    Update UserService.java

--- DistributedSystemsAssignment/src/main/java/gr/hua/dit/DistributedSystemsAssignment/service/UserService.java
+++ DistributedSystemsAssignment/src/main/java/gr/hua/dit/DistributedSystemsAssignment/service/UserService.java
@@ -1,14 +1,18 @@
 package gr.hua.dit.DistributedSystemsAssignment.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import gr.hua.dit.DistributedSystemsAssignment.dto.UserRegistrationDto;
+import gr.hua.dit.DistributedSystemsAssignment.entity.Application;
 import gr.hua.dit.DistributedSystemsAssignment.entity.User;
 
 public interface UserService extends UserDetailsService {
 	User save(UserRegistrationDto registrationDto);
 
 	List<User> getUsers();
+	
+	public Optional<User> getUser(int id);
 }
